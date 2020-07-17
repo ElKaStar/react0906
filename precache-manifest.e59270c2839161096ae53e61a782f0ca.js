9f1eb730eba49d421022c7dca15816a6c4af50ed
Elena Tikhonova <licose2019@gmail.com>
2020-07-17 23:06:25 +0300

    Updates

---
+++ precache-manifest.e59270c2839161096ae53e61a782f0ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6914248f5795c7c58a542803d496cc7d",
+    "url": "/react0906/index.html"
+  },
+  {
+    "revision": "5d2412e46d05f0c67cc4",
+    "url": "/react0906/static/css/main.ca79f573.chunk.css"
+  },
+  {
+    "revision": "899e2f9a230dac3c276f",
+    "url": "/react0906/static/js/2.1451c8ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react0906/static/js/2.1451c8ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0f01026d18012f363c5",
+    "url": "/react0906/static/js/3.e062fff6.chunk.js"
+  },
+  {
+    "revision": "8d9c66dc56141c4dfc88",
+    "url": "/react0906/static/js/4.30666f84.chunk.js"
+  },
+  {
+    "revision": "5d2412e46d05f0c67cc4",
+    "url": "/react0906/static/js/main.569762ed.chunk.js"
+  },
+  {
+    "revision": "ac0059286903a13a3ad4",
+    "url": "/react0906/static/js/runtime-main.d5859881.js"
+  }
+]);
